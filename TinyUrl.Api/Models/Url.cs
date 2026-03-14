005f951cc2190e4f71a09fb27ceedec4358e5b61
karthikmcakk <karthikmca.kk@gmail.com>
2026-03-14 23:12:04 +0530

    Add Url model

---
+++ TinyUrl.Api/Models/Url.cs
@@ -0,0 +1,16 @@
+﻿namespace TinyUrl.Api.Models;
+
+public class Url
+{
+    public int Id { get; set; }
+
+    public string OriginalUrl { get; set; } = string.Empty;
+
+    public string ShortCode { get; set; } = string.Empty;
+
+    public bool IsPrivate { get; set; }
+
+    public int Clicks { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
