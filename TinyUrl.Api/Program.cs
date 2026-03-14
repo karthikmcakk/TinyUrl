1c5c82ecdd8ad35819d376f62aaf6296232ca0a6
karthikmcakk <karthikmca.kk@gmail.com>
2026-03-15 00:15:38 +0530

    SQLite DB cofigure in program.cs file

--- TinyUrl.Api/Program.cs
+++ TinyUrl.Api/Program.cs
@@ -1,5 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using TinyUrl.Api.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseInMemoryDatabase("TinyUrlDb"));
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=tinyurl.db"));
+
 var app = builder.Build();
+
 
 app.MapGet("/", () => "Tiny URL API Running");
 
