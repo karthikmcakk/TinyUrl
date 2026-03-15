ece444b1fef624fbeb1ebad0cab72fe63f31c092
karthikmcakk <karthikmca.kk@gmail.com>
2026-03-15 17:37:24 +0530

    Add Migration and shotcode service file

---
+++ TinyUrl.Api/Services/ShortCodeService.cs
@@ -0,0 +1,18 @@
+﻿namespace TinyUrl.Api.Services
+{
+    public class ShortCodeService
+    {
+        private const string Characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        
+        public string GenerateShortCode(int length = 6)
+        {
+            var random = new Random();
+            var shortCode = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                shortCode[i] = Characters[random.Next(Characters.Length)];
+            }
+            return new string(shortCode);
+        }
+    }
+}
